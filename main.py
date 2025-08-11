eac754e4621771d7e9913167c6a44efd83f4f3fb
Guilherme Vieira <guirfv29@gmail.com>
2025-08-11 17:25:33 -0300

    Feat: LLM now can call some functions that I had defined

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import call_function, available_functions
 
 def main():
     load_dotenv()
@@ -29,12 +31,34 @@ def main():
     generate_content(client, messages, verbose)
 
 def generate_content(client, messages, verbose=False):
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages, 
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+            )
+        )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
 
+    if not response.function_calls:
+        return response.text
+    
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 if __name__ == "__main__":
     main()
